f824baf94b0de40359a021ea429daafea2a260f6
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-02-15 18:49:16 -0600

    Lab 04 parte 2 y 3 subidas

---
+++ Lab 4/Lab-04-José Andrés-1163926/Lab-04-Parte3-José Andrés/Lab-04-Parte3-José Andrés/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Declarar variable double
+        double energiaGenerada = 987.65;
+
+        // 2. Crear variable int
+        int energiaLimitada;
+
+        // 3. Casting explícito de double a int
+        energiaLimitada = (int)energiaGenerada;
+
+        // 4. Imprimir ambos valores
+        Console.WriteLine("Energía generada: " + energiaGenerada);
+        Console.WriteLine("Energía limitada: " + energiaLimitada);
+
+        Console.ReadLine();
+    }
+}
