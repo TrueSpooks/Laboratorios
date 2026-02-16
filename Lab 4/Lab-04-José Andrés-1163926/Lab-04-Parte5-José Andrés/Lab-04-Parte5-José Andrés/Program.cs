d604774240774581bc465dc41d0f596ddb1b0526
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-02-15 18:51:03 -0600

    Lab 04 parte 5, 6 y 7 subidas

---
+++ Lab 4/Lab-04-José Andrés-1163926/Lab-04-Parte5-José Andrés/Lab-04-Parte5-José Andrés/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declarar string señalOxigeno
+        string señalOxigeno = "true";
+
+        // Convertir a bool usando Convert.ToBoolean()
+        bool estadoOxigeno = Convert.ToBoolean(señalOxigeno);
+
+        // Declarar string temperaturaCabina
+        string temperaturaCabina = "22.8";
+
+        // Convertir a double usando Convert.ToDouble()
+        double tempoeratura = Convert.ToDouble(temperaturaCabina);
+
+        // Mostrar resultados
+        Console.WriteLine("Estado del oxígeno: " + estadoOxigeno);
+        Console.WriteLine("Temperatura de la cabina: " + temperaturaCabina);
+
+        Console.ReadLine();
+    }
+}
