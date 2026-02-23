2c56b90b1a29d8407f109649aabd652c046a5793
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-02-22 21:56:10 -0600

    Laboratorio 5 subido

---
+++ Lab 5/Lab-05-Parte 2- Jose Andres/Lab-05-Parte 2- Jose Andres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+{
+    {
+        int pin;
+
+        Console.Write("Ingrese el PIN: ");
+        pin = Convert.ToInt32(Console.ReadLine());
+
+        if (pin >= 1000 && pin <= 9999)
+            Console.WriteLine("PIN de 4 dígitos: OK.");
+        else
+            Console.WriteLine("PIN inválido: debe tener 4 dígitos.");
+
+        if (pin % 2 == 0)
+            Console.WriteLine("PIN par.");
+        else
+            Console.WriteLine("PIN impar.");
+
+        if (pin % 5 == 0)
+            Console.WriteLine("Múltiplo de 5.");
+        else
+            Console.WriteLine("No es múltiplo de 5.");
+
+        if ((pin >= 1000 && pin <= 9999) && (pin % 2 == 0) && (pin % 5 != 0))
+            Console.WriteLine("PIN aceptado por política.");
+        else
+            Console.WriteLine("PIN rechazado por política.");
+    }
+}
