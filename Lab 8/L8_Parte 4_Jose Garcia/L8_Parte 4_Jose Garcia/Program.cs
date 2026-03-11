3010694fdd321dac503c13501b8b4fb25ae8442a
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-03-11 09:28:49 -0600

    laboratorio 8 completo subido

---
+++ Lab 8/L8_Parte 4_Jose Garcia/L8_Parte 4_Jose Garcia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int cantidad = 0;
+        int positivos = 0;
+        int negativos = 0;
+        int suma = 0;
+
+        Console.Write("Ingrese un número: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (numero != 0)
+        {
+            cantidad++;
+            suma += numero;
+
+            if (numero > 0)
+            {
+                positivos++;
+            }
+            else if (numero < 0)
+            {
+                negativos++;
+            }
+
+            Console.Write("Ingrese otro número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Cantidad de números ingresados: " + cantidad);
+        Console.WriteLine("Cantidad de números positivos: " + positivos);
+        Console.WriteLine("Cantidad de números negativos: " + negativos);
+        Console.WriteLine("Suma total: " + suma);
+    }
+}
---
+++ Lab 8/L8_Parte 5_Jose Garcia/L8_Parte 5_Jose Garcia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+
+        Console.Write("Ingrese un número: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
