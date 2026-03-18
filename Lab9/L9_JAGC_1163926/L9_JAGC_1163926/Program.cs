12ccdf8ff76aa1d067aa67d931f67bae42d193c4
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-03-18 09:35:26 -0600

    Laboratorio 9 subido

---
+++ Lab9/L9_JAGC_1163926/L9_JAGC_1163926/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Variable
+        string nombre;
+
+        // Input
+        Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
+
+        // Llamada a los métodos
+        Saludo(nombre);
+        InfoCurso();
+
+        Console.ReadKey();
+    }
+
+    // Método 1: recibe el nombre como parámetro
+    static void Saludo(string nombre)
+    {
+        Console.WriteLine("Hola, " + nombre + "! Bienvenido a programación, la clase mortal!");
+    }
+
+    // Método 2: no recibe parámetros
+    static void InfoCurso()
+    {
+        Console.WriteLine("Curso: Pensamiento Computacional");
+        Console.WriteLine("Número del laboratorio: 9");
+    }
+}
---
+++ Lab9/L9_Parte 2_JAGC/L9_Parte 2_JAGC/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double lado, baseRect, alturaRect, baseTri, alturaTri;
+
+        // Área del cuadrado
+        Console.Write("Ingrese el lado del cuadrado: ");
+        lado = Convert.ToDouble(Console.ReadLine());
+        AreaCuadrado(lado);
+
+        // Área del rectángulo
+        Console.Write("\nIngrese la base del rectángulo: ");
+        baseRect = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese la altura del rectángulo: ");
+        alturaRect = Convert.ToDouble(Console.ReadLine());
+        AreaRectangulo(baseRect, alturaRect);
+
+        // Área del triángulo
+        Console.Write("\nIngrese la base del triángulo: ");
+        baseTri = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese la altura del triángulo: ");
+        alturaTri = Convert.ToDouble(Console.ReadLine());
+        AreaTriangulo(baseTri, alturaTri);
+
+        Console.ReadKey();
+    }
+
+    // Método 1
+    static void AreaCuadrado(double lado)
+    {
+        double area = lado * lado;
+        Console.WriteLine("Área del cuadrado: " + area);
+    }
+
+    // Método 2
+    static void AreaRectangulo(double baseRect, double alturaRect)
+    {
+        double area = baseRect * alturaRect;
+        Console.WriteLine("Área del rectángulo: " + area);
+    }
+
+    // Método 3
+    static void AreaTriangulo(double baseTri, double alturaTri)
+    {
+        double area = (baseTri * alturaTri) / 2;
+        Console.WriteLine("Área del triángulo: " + area);
+    }
+}
