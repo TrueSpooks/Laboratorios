3d24cf99f6a83af356429179da4d1d436541e0da
José Andrés García Calderón <jagcggvzmco@gmail.com>
2026-04-15 09:39:01 -0600

    Lab 11 parte 2 subido

---
+++ Lab 11/L11_JAGC_Parte 2/L11_JAGC_Parte 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string texto;
+        string invertido = "";
+
+        Console.Write("Ingrese una cadena: ");
+        texto = Console.ReadLine();
+
+        // Recorrer desde el final hasta el inicio
+        for (int i = texto.Length - 1; i >= 0; i--)
+        {
+            invertido += texto[i];
+        }
+
+        Console.WriteLine("Texto invertido: " + invertido);
+    }
+}
